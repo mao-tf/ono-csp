b6493a4803048119f8ea2b1ee6bdd0bd93bad673
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2026-07-06 16:33:08 +0900

    Add files via upload

--- legacy/ono_scripts/stepwise_optimization/step2_para.py
+++ legacy/ono_scripts/stepwise_optimization/step2_para.py
@@ -5,6 +5,9 @@ from utils import get_E
 import argparse
 import numpy as np
 from make_step2_para import exec_gjf
+import math
+import cmath
+import matplotlib.pyplot as plt
 
 def submit_process(args):## submission of the energy calculations jobs
     auto_dir = args.auto_dir
@@ -64,7 +67,6 @@ def listen(log_filepath_1,log_filepath_2):## checking whether the energy calcula
     else:
         isOver=True
     return isOver
-        
 
 def get_file_base_name(monomer_name,params_dict):
     a_ = params_dict['a']; b_ = params_dict['b']; theta = params_dict['theta']
@@ -86,10 +88,14 @@ def end_process(args):
     file_name_2 = file_base_name
     file_name_1 += '1.log'
     file_name_2 += '2.log'
-    log_filepath_1 = os.path.join(*[auto_dir,'gaussian',file_name_1])
-    log_filepath_2 = os.path.join(*[auto_dir,'gaussian',file_name_2])
-    E_list1=get_E(log_filepath_1)##t
-    E_list2=get_E(log_filepath_2)##p
+    while True:
+        log_filepath_1 = os.path.join(*[auto_dir,'gaussian',file_name_1])
+        log_filepath_2 = os.path.join(*[auto_dir,'gaussian',file_name_2])
+        E_list1=get_E(log_filepath_1)##t
+        E_list2=get_E(log_filepath_2)##p
+        if len(E_list1)==41 & len(E_list2)==41:
+            break
+        time.sleep(60)
     E_list_1=[]
     E_list_2=[]
     for i in range(len(E_list1)):
@@ -101,11 +107,54 @@ def end_process(args):
     z_list=[np.round(z,1) for z in np.linspace(-np.round(4,1),np.round(4,1),int(np.round(np.round(8,1)/0.1))+1)]
     para_list=[]
     for E1,E2,z in zip (E_list_1,E_list_2,z_list):
-        para_list.append((z,E1,E2))
+        para_list.append(z,E1,E2)
     df=pd.DataFrame(para_list,columns=['z','Et','Ep'])
     result_params_csv=os.path.join(auto_dir, 'step2_para.csv')
     df.to_csv(result_params_csv,index=False)
     
+def plot2d(args):
+    auto_dir = args.auto_dir
+    result_params_csv=os.path.join(auto_dir, 'step2_para.csv')
+    df=pd.read_csv(result_params_csv)
+    E_1d_list=[];z_list_t=[];z_list_p=[]
+    z_list=[np.round(z,1) for z in np.linspace(-np.round(4,1),np.round(4,1),int(np.round(np.round(8,1)/0.1))+1)]
+    for z1 in z_list:##t
+        for z2 in z_list:##p            
+            if ((z1-z2)>4.01) or (-4.01>(z1-z2)):
+                continue
+            else:
+                z_list_t.append(z1);z_list_p.append(z2)
+                Et1 = df[df['z']==z1]['Et'].values.tolist()[0]
+                Et2 = df[df['z']==round(z1-z2,1)]['Et'].values.tolist()[0]
+                Ep = df[df['z']==z2]['Ep'].values.tolist()[0]
+                E=2*(Et1+Et2+Ep)
+            E_1d_list.append(E)
+
+    init_params_csv=os.path.join(auto_dir, 'step2_init_params.csv')
+    df_init_params = pd.read_csv(init_params_csv)
+    df_init_params = df_init_params.iloc[0]
+    params_dict = df_init_params.to_dict()
+    a = params_dict['a']; b = params_dict['b']
+
+    x_list=[];y_list=[];para_list=[]
+    for zt,zp,E in zip(z_list_t,z_list_p,E_1d_list):
+        za=2*zt-zp;zb=zp
+        Z=1/math.sqrt(1+(za/a)**2+(zb/b)**2)
+        theta=np.degrees(math.acos(Z))
+        phi=cmath.phase(za/a+zb/b*1j)
+        x_list.append(theta*math.cos(phi))
+        y_list.append(theta*math.sin(phi))
+        para_list.append([])
+
+    plt.tick_params(labelsize=16)
+    plt.gca().set_aspect('equal')
+    plt.scatter(x_list,y_list,s=11,c=E_1d_list,cmap='coolwarm')
+    plt.xticks([-45,-30,-15,0,15,30,45])
+    plt.yticks([-30,-20,-10,0,10,20,30])
+    cbar=plt.colorbar()
+    cbar.ax.tick_params(labelsize=16)
+    plt.savefig(os.path.join(auto_dir,'result.png'),dpi=500)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
@@ -113,6 +162,7 @@ if __name__ == '__main__':
     parser.add_argument('--isTest',action='store_true')
     parser.add_argument('--isEnd',action='store_true')
     parser.add_argument('--isMain',action='store_true')
+    parser.add_argument('--plot',action='store_true')
     parser.add_argument('--auto-dir',type=str,help='path to dir which includes gaussian, gaussview and csv')
     parser.add_argument('--monomer-name',type=str,help='monomer name')
     parser.add_argument('--num-nodes',type=int,help='num nodes')
@@ -123,5 +173,7 @@ if __name__ == '__main__':
     submit_process(args)
     main_process(args)
     end_process(args)
+    if args.plot:
+        plot2d(args)
     print("----finish process----")
     
--- legacy/ono_scripts/stepwise_optimization/step3_para_vdw.py
+++ legacy/ono_scripts/stepwise_optimization/step3_para_vdw.py
@@ -1,54 +1,83 @@
-##エネルギー等計算の対象を確認　R3:t-shaped R4:slipped-parallel
+##エネルギー等計算の対象を確認　Rt:t-shaped Rp:slipped-parallel
 import numpy as np
-from tqdm import tqdm
-from make_step3_para import get_monomer_xyzR
-from scipy.ndimage.filters import maximum_filter
+import pandas as pd
+from utils import Rod
 
-def get_c_vec_vdw(monomer_name,R3,R4,a_,b_,theta):#,name_csv
+def get_monomer_xyzR1(monomer_name,Ta,Tb,Tc,A2,A3):## rotation and translation operations
+    T_vec = np.array([Ta,Tb,Tc])
+    df_mono=pd.read_csv('~/path/to/monomer/{}.csv'.format(monomer_name))
+    df_mono=df_mono[df_mono['Z']>0]
+    atoms_array_xyzR=df_mono[['X','Y','Z','R']].values
     
-    i=np.zeros(3); a=np.array([a_,0,2*R3-R4]); b=np.array([0,b_,R4]); t1=(a+b)/2;t2=(a-b)/2 ##ずらし方の定義
-    monomer_array_i = get_monomer_xyzR(monomer_name,0.,0.,0.,0.,theta)
+    ex = np.array([1.,0.,0.]); ey = np.array([0.,1.,0.]); ez = np.array([0.,0.,1.])
 
-    monomer_array_t = get_monomer_xyzR(monomer_name,0.,0.,0.,0.,-theta)
+    xyz_array = atoms_array_xyzR[:,:3]
+    xyz_array = np.matmul(xyz_array,Rod(-ex,A2).T)## A2 is a torsion angle in step2
+    xyz_array = np.matmul(xyz_array,Rod(ez,A3).T)## A3 is a half of dihedral angle
+    xyz_array = xyz_array + T_vec
+    R_array = atoms_array_xyzR[:,3].reshape((-1,1))
     
-    arr_list1=[[i,'p'],[b,'p'],[-b,'p'],[a,'p'],[-a,'p'],[t1,'t'],[-t1,'t'],[t2,'t'],[-t2,'t']]##層内の分子の座標とp or t
-    arr_list2=[[i,'t'],[b,'t'],[-b,'t'],[a,'t'],[-a,'t'],[t1,'p'],[-t1,'p'],[t2,'p'],[-t2,'p']]##層内の分子の座標とp or t
-    Rb_list=[np.round(Rb,1) for Rb in np.linspace(-np.round(b_/2,1),np.round(b_/2,1),int(np.round(2*np.round(b_/2,1)/0.1))+1)]
-    Ra_list=[np.round(Ra,1) for Ra in np.linspace(-np.round(a_/2,1),np.round(a_/2,1),int(np.round(2*np.round(a_/2,1)/0.1))+1)]
-    z_list=[];V_list=[];z_2dlist=[]
-    for Ra in tqdm(Ra_list):
-        z_maxlist=[]
-        for Rb in Rb_list:
+    return np.concatenate([xyz_array,R_array],axis=1)
+    
+def get_monomer_xyzR2(monomer_name,Ta,Tb,Tc,A2,A3):## rotation and translation operations
+    T_vec = np.array([Ta,Tb,Tc])
+    df_mono=pd.read_csv('~/path/to/monomer/{}.csv'.format(monomer_name))
+    df_mono=df_mono[df_mono['Z']<0]
+    atoms_array_xyzR=df_mono[['X','Y','Z','R']].values
+    
+    ex = np.array([1.,0.,0.]); ey = np.array([0.,1.,0.]); ez = np.array([0.,0.,1.])
+
+    xyz_array = atoms_array_xyzR[:,:3]
+    xyz_array = np.matmul(xyz_array,Rod(-ex,A2).T)## A2 is a torsion angle in step2
+    xyz_array = np.matmul(xyz_array,Rod(ez,A3).T)## A3 is a half of dihedral angle
+    xyz_array = xyz_array + T_vec
+    R_array = atoms_array_xyzR[:,3].reshape((-1,1))
+    
+    return np.concatenate([xyz_array,R_array],axis=1)
+    
+def get_c_vec_vdw(monomer_name,Rt,Rp,a_,b_,theta):#,name_csv
+    
+    i=np.zeros(3); a=np.array([a_,0,2*Rt-Rp]); b=np.array([0,b_,Rp]); t1=(a+b)/2;t2=(a-b)/2 ##ずらし方の定義
+    monomer_array_i0 = get_monomer_xyzR1(monomer_name,0.,0.,0.,0.,0.,theta);monomer_array_t0 = get_monomer_xyzR1(monomer_name,0.,0.,0.,0.,0.,-theta)
+    monomer_array_i = get_monomer_xyzR2(monomer_name,0.,0.,0.,0.,0.,theta);monomer_array_t = get_monomer_xyzR2(monomer_name,0.,0.,0.,0.,0.,-theta)
+    
+    arr_list1=[[i,'p'],[b,'p'],[-b,'p'],[a,'p'],[-a,'p'],[t1,'t'],[-t1,'t'],[t2,'t'],[-t2,'t']]##molecular layer with 9 atoms
+    arr_list2=[[i,'t'],[b,'t'],[-b,'t'],[a,'t'],[-a,'t'],[t1,'p'],[-t1,'p'],[t2,'p'],[-t2,'p']]##molecular layer with 9 atoms type2
+    cy_list=[np.round(cy,1) for cy in np.linspace(-np.round(b_/2,1),np.round(b_/2,1),int(np.round(2*np.round(b_/2,1)/0.1))+1)]
+    cx_list=[np.round(cx,1) for cx in np.linspace(-np.round(a_/2,1),np.round(a_/2,1),int(np.round(2*np.round(a_/2,1)/0.1))+1)]
+    para_list=[]
+    for cx in cx_list:
+        for cy in cy_list:
             z_max1=0
             for R,arr in arr_list1:
                 if arr=='t':
-                    monomer_array1=monomer_array_t
+                    monomer_array1=monomer_array_t0
                 elif arr=='p':
-                    monomer_array1=monomer_array_i
-                for x1,y1,z1,R1 in monomer_array1:#層内
+                    monomer_array1=monomer_array_i0
+                for x1,y1,z1,R1 in monomer_array1:#z1>0
                     x1,y1,z1=np.array([x1,y1,z1])+R
-                    for x2,y2,z2,R2 in monomer_array_i:#i0
-                        x2+=Ra
-                        y2+=Rb
-                        z2+=(2*R3-R4)*Ra/a_+R4*Rb/b_
+                    for x2,y2,z2,R2 in monomer_array_i:#i z2<0
+                        x2+=cx
+                        y2+=cy
+                        z2+=(2*Rt-Rp)*cx/a_+Rp*cy/b_
                         z_sq=(R1+R2)**2-(x1-x2)**2-(y1-y2)**2
                         if z_sq<0:
                             z_clps=0.0
                         else:
-                            z_clps=np.sqrt(z_sq)+z1-z2
+                            z_clps=np.sqrt(z_sq)+z1-z2 ## z1:underlayer(z>0) z2:overlayer(z<0) 
                         z_max1=max(z_max1,z_clps)
             z_max2=0
             for R,arr in arr_list2:
                 if arr=='t':
-                    monomer_array1=monomer_array_t
+                    monomer_array1=monomer_array_t0
                 elif arr=='p':
-                    monomer_array1=monomer_array_i
-                for x1,y1,z1,R1 in monomer_array1:#層内
+                    monomer_array1=monomer_array_i0
+                for x1,y1,z1,R1 in monomer_array1:#
                     x1,y1,z1=np.array([x1,y1,z1])+R
-                    for x2,y2,z2,R2 in monomer_array_t:#i0
-                        x2+=Ra
-                        y2+=Rb
-                        z2+=(2*R3-R4)*Ra/a_+R4*Rb/b_
+                    for x2,y2,z2,R2 in monomer_array_t:
+                        x2+=cx
+                        y2+=cy
+                        z2+=(2*Rt-Rp)*cx/a_+Rp*cy/b_
                         z_sq=(R1+R2)**2-(x1-x2)**2-(y1-y2)**2
                         if z_sq<0:
                             z_clps=0.0
@@ -56,16 +85,8 @@ def get_c_vec_vdw(monomer_name,R3,R4,a_,b_,theta):#,name_csv
                             z_clps=np.sqrt(z_sq)+z1-z2
                         z_max2=max(z_max2,z_clps)
             z_max=max(z_max1,z_max2)
-            z_list.append(z_max)
-            V_list.append(a_*b_*z_max)
-            z_maxlist.append(-z_max)
-        z_2dlist.append(z_maxlist)
-    return z_2dlist##ここでは層間距離を計算している
-
-def detect_peaks(image, filter_size):
-    local_max = maximum_filter(image, footprint=np.ones((filter_size, filter_size)), mode='constant')
-    detected_peaks = np.ma.array(image, mask=~(image == local_max))
-    return detected_peaks
-
-    
+            cz=round(z_max+(2*Rt-Rp)*cx/a_+Rp*cy/b_,1)
+            para_list.append([cx,cy,cz,z_max*a_*b_])
+    df=pd.DataFrame(para_list,columns=['cx','cy','cz','V'])
+    return df
 
